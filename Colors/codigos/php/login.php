eb4a8f6e48020da1a8339e054e908fe56776a0bf
MarcellaMel <104833591+MarcellaMel@users.noreply.github.com>
2023-10-17 10:52:17 -0300

    Update login.php

--- Colors/codigos/php/login.php
+++ Colors/codigos/php/login.php
@@ -4,10 +4,24 @@
     require_once("conexao.php"); //inclui o arquivo de conexao
     session_start(); //starta a session
     //recebendo usuario e senha vindo do login
-    $usuario = $conn->real_escape_string($_POST["emailUser"]);
+    $email = $conn->real_escape_string($_POST["emailUser"]);
     $senha = md5($_POST["senhaUser"]);
 
-    $sql = "SELECT * from usuario where emailUser = '$usuario' and password = '$senha'";
+    $sql = "SELECT * from usuario where emailUser = '$email' and senhaUser = '$senha'";
 
-    echo $sql;
+    //echo $sql;
+
+    $resultado = $conn->query($sql);
+
+if($resultado->num_rows > 0){
+    $dados_usuario = $resultado->fetch_assoc();
+    //preenchenr a session com os dados do usuario
+    $_SESSION["nome"] = $dados_usuario["nomeUser"];
+    $_SESSION["sobrenome"] = $dados_usuario["sobrenomeUser"];
+    header("location: selecionarPessoa.php");
+}else{
+    ?>
+    <script>window.history.back();</script>
+    <?php
+}
 ?>
