fcd230394e935c2ec1ba74e9e4f52967a30beb2a
MarcellaMel <104833591+MarcellaMel@users.noreply.github.com>
2023-10-17 10:52:03 -0300

    Update formLogin.php

--- Colors/codigos/php/formLogin.php
+++ Colors/codigos/php/formLogin.php
@@ -7,27 +7,25 @@
     <title>Login</title>
 </head>
 <body>
+    <div class= "container mt-5"> 
     <h2>Formulário de Login</h2>
     <form action="login.php" method="post">
     <fieldset class="ladoB">
 
-                    <div class="form-outline mb-4">
-                        <label for="emailUser">Email</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" id="emailUser" name="emailUser" required
-                                    placeholder="Digite seu email"><br><br>
-                            </div>
-                    </div>
+        <div class="form-outline mb-4">
+            <label for="emailUser">Email:</label>
+                    <input type="text" class="form-control" name="emailUser" id="emailUser" required placeholder="Digite seu email"><br><br>
+        </div>
 
-                    <div class="form-outline mb-4">
-                        <label for="senhaUser">Senha</label>
-                            <div class="col-sm-10">
-                                <input type="password" class="form-control" name="senhaUser" required
-                                    placeholder="Digite sua senha"><br><br>
-                            </div>
-                    </div>
-                    <input class="btn btn-primary" type="submit" value="Enviar">
-                    <input type="reset" class="btn btn-warning" value="Cancelar">
+        <div class="form-outline mb-4">
+            <label for="senhaUser">Senha:</label>
+                    <input type="password" class="form-control" name="senhaUser" required placeholder="Digite sua senha"><br><br>
+        </div>
+
+        <input type="submit" class="btn btn-primary" value="Enviar">
+        <input type="reset" class="btn btn-warning" value="Cancelar">
     </form>
+</div>
+
 </body>
 </html>
