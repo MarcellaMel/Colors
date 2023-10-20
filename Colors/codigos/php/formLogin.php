217d9054c2f1b96ea3d504f06009091a7b6a907a
MarcellaMel <104833591+MarcellaMel@users.noreply.github.com>
2023-10-20 14:50:15 -0300

    Update formLogin.php

--- Colors/codigos/php/formLogin.php
+++ Colors/codigos/php/formLogin.php
@@ -1,31 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 <body>
-    <div class= "container mt-5"> 
-    <h2>Formulário de Login</h2>
+    <h2>Login</h2>
     <form action="login.php" method="post">
-    <fieldset class="ladoB">
-
-        <div class="form-outline mb-4">
-            <label for="emailUser">Email:</label>
-                    <input type="text" class="form-control" name="emailUser" id="emailUser" required placeholder="Digite seu email"><br><br>
-        </div>
-
-        <div class="form-outline mb-4">
-            <label for="senhaUser">Senha:</label>
-                    <input type="password" class="form-control" name="senhaUser" required placeholder="Digite sua senha"><br><br>
-        </div>
-
-        <input type="submit" class="btn btn-primary" value="Enviar">
-        <input type="reset" class="btn btn-warning" value="Cancelar">
+        <label for="username">Nome de Usuário:</label>
+        <input type="text" id="username" name="username" required><br><br>
+        <label for="password">Senha:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Login">
     </form>
-</div>
-
 </body>
 </html>
