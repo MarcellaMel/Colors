05c678b6270dd5740027f11aabb7502d806b33c0
MarcellaMel <104833591+MarcellaMel@users.noreply.github.com>
2023-11-21 10:24:51 -0300

    Update ems.php

---
+++ Colors/codigos/php/ems.php
@@ -0,0 +1,18 @@
+<?php
+include_once("conexao.php");
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esqueci minha senha</title>
+</head>
+<body>
+    <h1>Esqueci minha senha, gata! E agora?!</h1>
+</body>
+</html>
