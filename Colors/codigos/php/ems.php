1aa2519efdfb4c92ef488052a6153470b8fb526b
MarcellaMel <104833591+MarcellaMel@users.noreply.github.com>
2024-01-05 07:21:43 -0300

    Update ems.php

--- Colors/codigos/php/ems.php
+++ Colors/codigos/php/ems.php
@@ -1,58 +1,52 @@
+
 <?php
 session_start();
-include_once("conexao.php");
-?>
+//include_once("conexao.php");
 
+// Simulação de banco de dados para armazenar usuários e senhas
+$users = array(
+    'usuario1' => array('senha' => 'senha1'),
+    'usuario2' => array('senha' => 'senha2')
+);
+
+// Simulação de banco de dados para armazenar códigos de redefinição de senha
+$resetCodes = array();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $usuario = $_POST['emailUser'];
+    $codigo = $_POST['codigo'];
+    $novaSenha = $_POST['novaSenha'];
+
+    if (isset($users[$usuario]) && isset($resetCodes[$usuario]) && $resetCodes[$usuario] === $codigo) {
+        // Atualizar a senha do usuário no banco de dados
+        $users[$usuario]['senha'] = $novaSenha;
+
+        // Limpar o código de redefinição de senha
+        unset($resetCodes[$usuario]);
+
+        echo "Senha redefinida com sucesso!";
+    } else {
+        echo "Código inválido ou expirado!";
+    }
+    //Ta sem o nosso html ainda
+    //Tava "funcionando". Ele criava um novo usuario com o email normal e a senha nova, mexi começou a dar ruim
+}
+?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Esqueci minha senha</title>
+    <title>Redefinir Senha</title>
 </head>
 <body>
-    <h1>Esqueci minha senha, gata! E agora?!</h1>
-
-    
-
-    <?php
-    $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-
-    if(!empty($dados['sendRecuperar'])){
-        var_dump($dados);
-                $query_usuario = "SELECT emailUser
-                        FROM usuario
-                        WHERE emailUser = :emailUser
-                        LIMIT 1";
-        $result_usuario = $conn->prepare($query_usuario);
-        $result_usuario->bindParam(':emailUser', $dados['emailUser'], PDO::PARAM_STR);
-        $result_usuario->execute();
-
-        if(($result_usuario) AND ($result_usuario->rowCount() !=0)){
-    
-        }else{
-            $_SESSION['msg'] = "<p style='color: #ff0000'>Error: Usuario nao encontrado!</p>";
-        }
-    }
-
-    if(isset($_SESSON['msg'])){
-        echo $_SESSION['msg'];
-        unset($_SESSION['msg']);
-    }
-
-    ?>
-
-    <form method="POST" action="">
-        <label>Email</label>
-        <input type="text"  name="emailUser" id="emailUser" class="form-control" placeholder="Digite seu email cadastrado"
-        value="<?php if(isset($dados['emailUser'])){ echo $dados['emailUser'];}?>"<br><br>
-    
-        <input type="submit" value="Recuperar" name="sendRecuperar" id="sendRecuperar">
+    <h1>Redefinir Senha</h1>
+    <form method="post" action="">
+        <input type="text" name="usuario" value="<?php echo $_GET['usuario']; ?>">
+        <input type="text" name="codigo" value="<?php echo $_GET['codigo']; ?>">
+        <label for="novaSenha">Nova Senha:</label>
+        <input type="password" id="novaSenha" name="novaSenha" required>
+        <button type="submit">Redefinir Senha</button>
     </form>
-
 </body>
-< !-- TA DANDO ERRO POR ENQUANTO
-array(2) { ["emailUser"]=> string(17) "teste44@gmail.com" ["sendRecuperar"]=> string(9) "Recuperar" }
-Fatal error: Uncaught mysqli_sql_exception: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ':emailUser LIMIT 1' at line 3 in C:\Programas\xampp\htdocs\Colors-main\Colors-main\Colors\codigos\php\ems.php:28 Stack trace: #0 C:\Programas\xampp\htdocs\Colors-main\Colors-main\Colors\codigos\php\ems.php(28): mysqli->prepare('SELECT idUser, ...') #1 {main} thrown in C:\Programas\xampp\htdocs\Colors-main\Colors-main\Colors\codigos\php\ems.php on line 28-- >
 </html>
