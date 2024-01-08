da78d32619bb4e397a6364b85e3c21ac904b69b4
Lara Nogueira <laracostanog@gmail.com>
2024-01-08 16:48:18 -0300

    Update atualizaUser.php

--- Colors/codigos/php/atualizaUser.php
+++ Colors/codigos/php/atualizaUser.php
@@ -6,30 +6,31 @@
 $userId= $_GET['idUser'];
 
 //receber os dados do formulário
-  $nome = $_SESSION["nomeUser"];
-  $sobrenome = $_SESSION["sobrenomeUser"];
-  $email = $_SESSION["emailUser"];
-  
-  $telefone = $_SESSION["telUser"];
-  $nascimento = $_SESSION["nascimentoUser"];
+  $nome = $_POST["nomeUser"];
+  $sobrenome = $_POST["sobrenomeUser"];
+  $telefone = $_POST["telUser"];
  
 
 
 //inserir os dados no banco de dados
 
-                        //$result = $conn->query("SELECT COUNT(*) FROM usuario WHERE emailUser = '{$email}'");
                         //$row = $result->fetch_row();
-                          $sql = "UPDATE usuario SET  nomeUser = '$nome', sobrenomeUser ='$sobrenome', emailUser = '$email',telUser = '$telefone', nascimentoUser = '$nascimento';";
+                          $sql = "UPDATE usuario SET  nomeUser = '$nome', sobrenomeUser ='$sobrenome', telUser = '$telefone' WHERE idUser = '$userId';";
 
                           if($conn->query($sql) === TRUE){
-                            echo ("Dados inseridos com sucesso!");
-                            header("location: ../html/quizz.html");
+                            ?>
+                            <?php session_abort()?>
+                            
+                            <script> alert("Dados inseridos com sucesso!");</script>
+                            
+                            <?php
+                            header("location: editarPessoa.php");
                            
                           } else {
                             echo ( "Erro ao inserir dados: " . $conn->error);
                            
                           }
-                        
+                          
                         
                         
  
