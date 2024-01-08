56665d93eb4ade635fcebcb262c38bf96cf41634
MarcellaMel <104833591+MarcellaMel@users.noreply.github.com>
2024-01-08 11:32:12 -0300

    Update processa_cadastro.php

--- Colors/codigos/php/processa_cadastro.php
+++ Colors/codigos/php/processa_cadastro.php
@@ -15,27 +15,26 @@
   //echo $nome;
 
 //inserir os dados no banco de dados
+  $result = $conn->query("SELECT COUNT(*) FROM usuario WHERE emailUser = '{$email}'");
+  $row = $result->fetch_row();
 
-                        $result = $conn->query("SELECT COUNT(*) FROM usuario WHERE emailUser = '{$email}'");
-                        $row = $result->fetch_row();
-                       
-                        if ($row[0] > 0) {
-                          echo '<script>alert("Não foi possível realizar seu cadastro ;(")</script>';
+   //confere se ja existe esse login                    
+    if ($row[0] > 0) {
+    echo '<script>alert("Não foi possível realizar seu cadastro ;(")</script>';
                           
-                        } else {
-                          $sql = "INSERT INTO usuario (nomeUser, sobrenomeUser, emailUser, senhaUser, telUser, nascimentoUser, generoUser) VALUES ('$nome', '$sobrenome', '$email', '$senha', '$telefone', '$nascimento', '$gen')";
+    } else {
+    $sql = "INSERT INTO usuario (nomeUser, sobrenomeUser, emailUser, senhaUser, telUser, nascimentoUser, generoUser) VALUES ('$nome', '$sobrenome', '$email', '$senha', '$telefone', '$nascimento', '$gen')";
 
-                          if($conn->query($sql) === TRUE){
-                            echo ("Dados inseridos com sucesso!");
-                            header("location: ../html/quizz.html");
+    if($conn->query($sql) === TRUE){
+    echo ("Dados inseridos com sucesso!");
+    header("location: ../html/quizz.html");
                            
-                          } else {
-                            echo ( "Erro ao inserir dados: " . $conn->error);
+      } else {
+         echo ( "Erro ao inserir dados: " . $conn->error);
                            
-                          }
+              }
                         
-                        }
+    }
                         
- 
   $conn->close();
   ?>
